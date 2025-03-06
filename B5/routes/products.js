1301f7b06eb76b4db2de620f6b9e095e743cfef4
vtchien2223 <vchien070@gmail.com>
2025-03-06 15:59:33 +0700

    update

--- B5/routes/products.js
+++ B5/routes/products.js
@@ -3,38 +3,44 @@ const { ConnectionCheckOutFailedEvent } = require('mongodb');
 var router = express.Router();
 let productModel = require('../schemas/product')
 
-function buildQuery(obj){
-  console.log(obj);
+function buildQuery(obj) {
+  console.log("req.query:", obj); 
   let result = {};
-  if(obj.name){
-    result.name=new RegExp(obj.name,'i');
+  
+
+  if (obj.name) {
+    result.name = new RegExp(obj.name, 'i');
   }
-  result.price = {};
-  if(obj.price){
-    if(obj.price.$gte){
-      result.price.$gte = obj.price.$gte;
-    }else{
-      result.price.$gte = 0
-    }
-    if(obj.price.$lte){
-      result.price.$lte = obj.price.$lte;
-    }else{
-      result.price.$lte = 10000;
+  
+  if (obj.price && typeof obj.price === 'object' && Object.keys(obj.price).length > 0) {
+    result.price = {};
+    
+    if (obj.price.$gte) {
+      result.price.$gte = Number(obj.price.$gte); 
     }
     
+    if (obj.price.$lte) {
+      result.price.$lte = Number(obj.price.$lte); 
+    }
   }
+  
   return result;
 }
 
+
 router.get('/', async function(req, res, next) {
-  
-
-  let products = await productModel.find(buildQuery(req.query));
-
-  res.status(200).send({
-    success:true,
-    data:products
-  });
+  try {
+    let products = await productModel.find(buildQuery(req.query));
+    res.status(200).send({
+      success: true,
+      data: products
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: error.message
+    });
+  }
 });
 router.get('/:id', async function(req, res, next) {
   try {
@@ -72,6 +78,7 @@ router.post('/', async function(req, res, next) {
     });
   }
 });
+
 router.put('/:id', async (req, res) => {
   try {
       let updatedProduct = await productModel.findOneAndUpdate(
@@ -97,6 +104,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+
 router.delete('/:id', async (req, res) => {
   try {
       let deletedProduct = await productModel.findOneAndUpdate(
@@ -112,7 +120,7 @@ router.delete('/:id', async (req, res) => {
       }
       res.status(200).json({ 
           success: true, 
-          message: 'Sản phẩm đã bị xóa (mềm)', 
+          message: 'Sản phẩm đã bị xóa ', 
           data: deletedProduct 
       });
   } catch (error) {
